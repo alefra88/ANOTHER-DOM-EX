26b76655ac21d63fb4f785d3a7ff2e6ee8662048
alefra88 <efra.rlz@gmail.com>
2023-02-13 02:05:04 -0600

    add project to index.html

--- scripts/script.js
+++ scripts/script.js
@@ -11,45 +11,45 @@ d.body.insertBefore(title, d.body.firstChild);
 // cards container
 // generating card
 const createCard = (title, image, link) => {
-    // making div to contain cards
-    let cardContainer = document.createElement("div");
-    cardContainer.classList.add("card");
+  // making div to contain cards
+  let cardContainer = document.createElement("div");
+  cardContainer.classList.add("card");
 
-    // make h2 elem to title card
-    let cardTitle = document.createElement("h2");
-    cardTitle.textContent = title;
+  // make h2 elem to title card
+  let cardTitle = document.createElement("h2");
+  cardTitle.textContent = title;
 
-    // make img elem to image card
-    let cardImage = document.createElement("img");
-    cardImage.src = image;
+  // make img elem to image card
+  let cardImage = document.createElement("img");
+  cardImage.src = image;
 
-    // add h2 and img elem to card container
-    cardContainer.appendChild(cardTitle);
-    cardContainer.appendChild(cardImage);
+  // add h2 and img elem to card container
+  cardContainer.appendChild(cardTitle);
+  cardContainer.appendChild(cardImage);
 
-    // add click event of each card
-    cardContainer.addEventListener("click", function () {
-        window.location.href = link;
-    });
+  // add click event of each card
+  cardContainer.addEventListener("click", function () {
+    window.location.href = link;
+  });
 
-    // return cards
-    return cardContainer;
+  // return cards
+  return cardContainer;
 };
 // Usa la función para crear tres tarjetas
 let card1 = createCard(
-    "Color Flipper",
-    "./assets/firstaplication.png",
-    "./color-flipper/index.html"
+  "Color Flipper",
+  "./assets/firstaplication.png",
+  "./color-flipper/index.html"
 );
 let card2 = createCard(
-    "Counter",
-    "./assets/counter.png",
-    "./counter/index.html"
+  "Counter",
+  "./assets/counter.png",
+  "./counter/index.html"
 );
 let card3 = createCard(
-    "Título de la tarjeta 3",
-    "https://via.placeholder.com/150x150",
-    "content3.html"
+  "Card Reviews",
+  "./assets/reviews.png",
+  "./review/index.html"
 );
 
 // Añade las tarjetas a un elemento en el HTML
@@ -60,5 +60,5 @@ cardContainer.appendChild(card3);
 
 //DARK MODE
 d.getElementById("btn").addEventListener("click", function () {
-    d.body.classList.toggle("dark-mode");
+  d.body.classList.toggle("dark-mode");
 });
