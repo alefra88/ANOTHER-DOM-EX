24fb5bed2b14740e52efc78c28a07852e2a2de33
alefra88 <efra.rlz@gmail.com>
2023-02-15 23:41:26 -0600

    add quote

--- scroll-effect/app.js
+++ scroll-effect/app.js
@@ -82,3 +82,68 @@ scrollLinks.forEach((link) => {
   });
 });
 // calculate heights
+
+// insertar texto
+const texts = {
+  text1:
+    "By combining my skills as a developer with my knowledge of anthropology, I am able to create software that is truly human-centered and transformative.",
+  text2:
+    "I am committed to creating software that reflects the values and beliefs of the people who use it, and I approach each project with a deep respect for their needs and desires.",
+  text3:
+    "Believe in the power of code to change the world, and trust in me to help bring your vision to life",
+  text4: "With every line of code, I strive to make the world a better place.",
+  text5:
+    "As a software developer, I am not just solving problems; I am creating opportunities.",
+  text6:
+    "I believe that innovation is the key to unlocking the full potential of technology, and I am committed to being a part of that process.",
+  text7:
+    "I am passionate about using technology to drive positive change and bring new ideas to life.",
+  text8:
+    "In a world of constant change, I am dedicated to staying on the cutting edge of technology and pushing boundaries with my work.",
+  text9:
+    "With every project I undertake, I bring a commitment to excellence and a passion for innovation.",
+  text10:
+    "I am not just a developer; I am a problem solver, a creative thinker, and a visionary.",
+  text11:
+    "I believe that great software is not just about writing code; it's about crafting experiences that inspire and delight.",
+  text12:
+    "I am constantly learning and growing, and I am committed to using my skills to make a difference in the world.",
+  text13:
+    "As a software developer, I am not limited by what is, but inspired by what could be.",
+  text14:
+    "Through software development, I am able to explore and understand the cultural and social implications of technology in our modern world.",
+  text15:
+    "As a software developer, I strive to create tools that are intuitive and accessible, meeting the needs of diverse communities and cultures.",
+  text16:
+    "My work as a software developer is informed by a deep understanding of the ways in which technology shapes human behavior and experience.",
+  text17:
+    "As an anthropologist and a developer, I approach every project with a keen eye for the cultural and social context in which it will be used, and strive to create solutions that are both meaningful and effective.",
+  text18:
+    "Through my work as a software developer, I am able to bridge the gap between technology and humanity, creating tools that are responsive to the needs and desires of the people who use them.",
+  text19:
+    "I believe that software development is an inherently social and cultural activity, and that understanding the complex systems in which technology operates is crucial to creating meaningful solutions.",
+  text20:
+    "As an anthropologist who has studied the development of software, I am committed to creating solutions that are not only functional, but also meaningful and empowering for those who use them.",
+  text21:
+    "My work as a software developer is driven by a deep curiosity and fascination with the ways in which technology is transforming our world, and a commitment to using that technology for good.",
+};
+// Obtener el elemento padre
+const container = document.querySelector(".container");
+
+// Crear el nuevo elemento p
+const newParagraph = document.createElement("p");
+
+// Agregar el contenido al nuevo elemento p
+// newParagraph.textContent = "Este es un nuevo párrafo";
+
+// Obtener los elementos a
+const link = document.querySelector("#btnsee");
+
+// Insertar el nuevo elemento p después del h1 y antes del a
+
+const randomText =
+  texts[
+    Object.keys(texts)[Math.floor(Math.random() * Object.keys(texts).length)]
+  ];
+newParagraph.textContent = randomText;
+container.insertBefore(newParagraph, link);
