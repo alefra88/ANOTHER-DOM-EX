119b1e926b5fb48480c5a4a0e0e104b479cbd258
alefra88 <efra.rlz@gmail.com>
2023-02-16 04:19:54 -0600

    counter of article

--- scripts/script.js
+++ scripts/script.js
@@ -71,7 +71,7 @@ let card10 = createCard(
   "./scroll-effect/index.html"
 );
 let card11 = createCard("About", "./assets/about.png", "./about/index.html");
-
+// let card12 = createCard("about", "./assets/about.png", "./about/index.html");
 // Añade las tarjetas a un elemento en el HTML
 let cardContainer = document.getElementById("card-container");
 cardContainer.appendChild(card1);
